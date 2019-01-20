7b4554bda3c30e3c4b93096bdf0d90bb8fbd3ec0
Glauco Pater <>
2019-01-20 15:01:50 +0100

    CSS Review

--- itinerary-timeline/src/components/Overview/Overview.jsx
+++ itinerary-timeline/src/components/Overview/Overview.jsx
@@ -3,10 +3,8 @@ import "./Overview.scss";
 
 export default function Overview (props) {
     return (
-        <div className="Overview">
-            <h1 className="header">Overview</h1>
-            <div className="from-to">
-                {props.origin_iata}-{props.destination_iata} 
-            </div>
-        </div>) 
+        <h1 className="Overview">
+            <button className="back fa fa-chevron-left"/>
+            Overview   
+        </h1>) 
 }
